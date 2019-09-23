dea38cf5b6574399a88202dc66ffdd53a0ab4cd0
José Costa Teixeira <jose.a.teixeira@gmail.com>
2019-09-23 15:55:49 +0200

    Change to new Template

--- _genonce.bat
+++ _genonce.bat
@@ -1,3 +1,25 @@
-SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8 -DupdatePublisher=N
-java -jar framework\ant\ant-launcher.jar -buildfile framework/build.xml
-pause
+@echo off
+
+echo Checking internet connection...
+Ping tx.fhir.org -n 1 -w 1000 | findstr TTL && goto isonline
+echo We're offline...
+set txoption=-tx XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+goto igpublish
+:isonline
+echo We're online
+set txoption=
+
+:igpublish
+
+
+@SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
+
+If exist "input-cache\org.hl7.fhir.publisher.jar" (
+   JAVA -jar input-cache/org.hl7.fhir.publisher.jar -ig ig.ini  
+) ELSE If exist "..\org.hl7.fhir.publisher.jar" (
+   JAVA -jar ../org.hl7.fhir.publisher.jar -ig ig.ini 
+   ) Else (
+@ECHO IG Publisher NOT FOUND in input-cache or parent folder... aborting
+ )
+
+@PAUSE
